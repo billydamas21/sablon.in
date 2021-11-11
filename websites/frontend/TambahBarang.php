e9c95c4daf17cb577a6a1ece05cc457d27d30f09
Farul Wananda <farulahmad2002@gmail.com>
2021-11-11 17:27:09 +0700

    update tampilan tambah & update barang

--- websites/frontend/TambahBarang.php
+++ websites/frontend/TambahBarang.php
@@ -1,3 +1,4 @@
+<?php require '../backend/BarangInsert.php' ?>
 <?php include 'templates/headerLink.php' ?>
 <?php include 'templates/topbar.php' ?>
 <?php include 'templates/sidebar.php' ?>
@@ -14,22 +15,25 @@
                         <h4>Kelola Barang</h4>
                     </div>
                     <div class="card-body">
-                        <form action="" class=" col-6 mx-auto">
+                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class=" col-6 mx-auto">
                             <div class="section-title mt-0">Tambah Barang</div>
-                            <div class="form-group">
-                                <label>Nama Barang</label>
-                                <input type="text" class="form-control" placeholder="Nama Barang" required>
+                            <div class="form-group" <?php echo (!empty($Nama_barang_alert)) ? 'has-error' : ''; ?>>
+                                <label for="Nama_barang">Nama Barang</label>
+                                <input type="text" class="form-control" name="Nama_barang" id="Nama_barang" placeholder="Nama Barang" value="<?php echo $Nama_barang; ?>" required>
+                                <span class=" help-block text-danger"><?php echo $Nama_barang_alert; ?></span>
                             </div>
-                            <div class="form-group">
-                                <label>Jenis Barang</label>
-                                <input type="text" class="form-control" placeholder="Jenis Barang" required>
+                            <div class="form-group" <?php echo (!empty($Jenis_barang_alert)) ? 'has-error' : ''; ?>>
+                                <label for="Jenis_barang">Jenis Barang</label>
+                                <input type="text" class="form-control" name="Jenis_barang" id="Jenis_barang" placeholder="Jenis Barang" value="<?php echo $Jenis_barang; ?>" required>
+                                <span class=" help-block text-danger"><?php echo $Jenis_barang_alert; ?></span>
                             </div>
-                            <div class="form-group">
-                                <label>Jumlah Barang</label>
-                                <input type="number" class="form-control" placeholder="Jumlah Barang" required>
+                            <div class="form-group" <?php echo (!empty($Jumlah_alert)) ? 'has-error' : ''; ?>>
+                                <label for="Jumlah">Jumlah Barang</label>
+                                <input type="number" class="form-control" name="Jumlah" id="Jumlah" placeholder="Jumlah Barang" value="<?php echo $Jumlah; ?>" required>
+                                <span class=" help-block text-danger"><?php echo $Jumlah_alert; ?></span>
                             </div>
                             <div class="text-right">
-                                <button class="btn btn-success mr-1" type="submit">
+                                <button class="btn btn-success mr-1" type="submit" name="TambahBarang" value="submit">
                                     Tambah
                                 </button>
                                 <a href="KelolaBarang.php" class="btn btn-secondary">Back</a>
--- websites/frontend/UpdateBarang.php
+++ websites/frontend/UpdateBarang.php
@@ -1,3 +1,4 @@
+<?php require '../backend/BarangUpdate.php' ?>
 <?php include 'templates/headerLink.php' ?>
 <?php include 'templates/topbar.php' ?>
 <?php include 'templates/sidebar.php' ?>
@@ -14,22 +15,28 @@
                         <h4>Kelola Barang</h4>
                     </div>
                     <div class="card-body">
-                        <form action="" class=" col-6 mx-auto">
+                        <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post" class=" col-6 mx-auto">
                             <div class="section-title mt-0">Update Barang</div>
-                            <div class="form-group">
+                            <div class="form-group" <?php echo (!empty($Nama_barang_alert)) ? 'has-error' : ''; ?>>
                                 <label>Nama Barang</label>
-                                <input type="text" class="form-control" placeholder="Nama Barang" required>
+                                <input type="text" class="form-control" placeholder="Nama Barang" name="Nama_barang" value="<?php echo $Nama_barang; ?>" required>
+                                <span class=" help-block text-danger"><?php echo $Nama_barang_alert; ?></span>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group" <?php echo (!empty($Jenis_barang_alert)) ? 'has-error' : ''; ?>>
                                 <label>Jenis Barang</label>
-                                <input type="text" class="form-control" placeholder="Jenis Barang" required>
+                                <input type="text" class="form-control" placeholder="Jenis Barang" name="Jenis_barang" value="<?php echo $Jenis_barang; ?>" required>
+                                <span class=" help-block text-danger"><?php echo $Jenis_barang_alert; ?></span>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group" <?php echo (!empty($Jumlah_alert)) ? 'has-error' : ''; ?>>
                                 <label>Jumlah Barang</label>
-                                <input type="number" class="form-control" placeholder="Jumlah Barang" required>
+                                <input type="number" class="form-control" placeholder="Jumlah Barang" name="Jumlah" value="<?php echo $Jumlah; ?>" required>
+                                <span class=" help-block text-danger"><?php echo $Jumlah_alert; ?></span>
+                            </div>
+                            <div>
+                                <input type="hidden" name="id" value="<?php echo $id; ?>" />
                             </div>
                             <div class="text-right">
-                                <button class="btn btn-warning mr-1" type="submit">
+                                <button class="btn btn-warning mr-1" type="submit" value="submit">
                                     Update
                                 </button>
                                 <a href="KelolaBarang.php" class="btn btn-secondary">Back</a>
