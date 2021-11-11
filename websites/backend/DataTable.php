6588047e5cff02a6500a84f24895047fe096ea8d
Farul Wananda <farulahmad2002@gmail.com>
2021-11-11 16:35:06 +0700

    Update Fitur Pagination Barang dan Riwayat

--- websites/backend/DataTable.php
+++ websites/backend/DataTable.php
@@ -1,4 +1,22 @@
 <?php
 // memanggil function query
-require 'QueryData.php';
+require 'Functions.php';
+// Query Data akun
 $KelolaAkun = query("SELECT * FROM users");
+$JumlahAkun = count(query("SELECT * FROM users"));
+
+// Query Data + Pagination barang
+$JumlahDataPerHalamanBarang = 10;
+$JumlahDataBarang = count(query("SELECT * FROM kelola_barang"));
+$JumlahHalamanBarang = ceil($JumlahDataBarang / $JumlahDataPerHalamanBarang);
+$HalamanAktifBarang = (isset($_GET["halaman"])) ? $_GET["halaman"] : 1;
+$AwalDataBarang = ($JumlahDataPerHalamanBarang * $HalamanAktifBarang) - $JumlahDataPerHalamanBarang;
+$Barang = query("SELECT * FROM kelola_barang LIMIT $AwalDataBarang, $JumlahDataPerHalamanBarang");
+
+// Query Data + Pagination Riwayat
+$JumlahDataPerHalamanRiwayat = 10;
+$JumlahDataRiwayat = count(query("SELECT * FROM riwayat_barang"));
+$JumlahHalamanRiwayat = ceil($JumlahDataRiwayat / $JumlahDataPerHalamanRiwayat);
+$HalamanAktifRiwayat = (isset($_GET["halaman"])) ? $_GET["halaman"] : 1;
+$AwalDataRiwayat = ($JumlahDataPerHalamanRiwayat * $HalamanAktifRiwayat) - $JumlahDataPerHalamanRiwayat;
+$Riwayat = query("SELECT * FROM riwayat_barang LIMIT $AwalDataRiwayat, $JumlahDataPerHalamanRiwayat");
