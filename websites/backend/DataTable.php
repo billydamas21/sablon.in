80f2d75487f361ebcb9d514313aa95c5d2a66180
Farul Wananda <farulahmad2002@gmail.com>
2021-11-12 02:16:41 +0700

    update query database & tabel barang

--- websites/backend/DataTable.php
+++ websites/backend/DataTable.php
@@ -1,22 +1,20 @@
 <?php
 // memanggil function query
 require 'Functions.php';
-// Query Data akun
+
+// Query Data akun + Jumlah data
+
 $KelolaAkun = query("SELECT * FROM users");
-$JumlahAkun = count(query("SELECT * FROM users"));
+$JumlahDataAkun = count(query("SELECT * FROM users"));
 
-// Query Data + Pagination barang
-$JumlahDataPerHalamanBarang = 10;
+
+$Barang = query("SELECT * FROM kelola_barang");
 $JumlahDataBarang = count(query("SELECT * FROM kelola_barang"));
-$JumlahHalamanBarang = ceil($JumlahDataBarang / $JumlahDataPerHalamanBarang);
-$HalamanAktifBarang = (isset($_GET["halaman"])) ? $_GET["halaman"] : 1;
-$AwalDataBarang = ($JumlahDataPerHalamanBarang * $HalamanAktifBarang) - $JumlahDataPerHalamanBarang;
-$Barang = query("SELECT * FROM kelola_barang LIMIT $AwalDataBarang, $JumlahDataPerHalamanBarang");
 
-// Query Data + Pagination Riwayat
-$JumlahDataPerHalamanRiwayat = 10;
+
+$Riwayat = query("SELECT * FROM riwayat_barang");
 $JumlahDataRiwayat = count(query("SELECT * FROM riwayat_barang"));
-$JumlahHalamanRiwayat = ceil($JumlahDataRiwayat / $JumlahDataPerHalamanRiwayat);
-$HalamanAktifRiwayat = (isset($_GET["halaman"])) ? $_GET["halaman"] : 1;
-$AwalDataRiwayat = ($JumlahDataPerHalamanRiwayat * $HalamanAktifRiwayat) - $JumlahDataPerHalamanRiwayat;
-$Riwayat = query("SELECT * FROM riwayat_barang LIMIT $AwalDataRiwayat, $JumlahDataPerHalamanRiwayat");
+
+
+$Ulasan = query("SELECT * FROM kelola_ulasan");
+$JumlahDataRiwayat = count(query("SELECT * FROM kelola_ulasan"));
--- websites/frontend/KelolaBarang.php
+++ websites/frontend/KelolaBarang.php
@@ -14,66 +14,39 @@
                     <div class="card card-primary">
                         <div class="card-header">
                             <h4>Tabel Barang</h4>
-                            <div class="card-header-form">
-                                <form>
-                                    <input type="text" name="searchBarang" class="form-control" placeholder="Search">
-                                </form>
-                            </div>
                         </div>
                         <div class="card-body">
                             <a href="TambahBarang.php" class="btn btn-success mb-3">Tambah Data</a>
                             <div class="table-responsive">
-                                <table class="table table-bordered table-md text-center">
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Nama Barang</th>
-                                        <th>Jenis Barang</th>
-                                        <th>Jumlah</th>
-                                        <th>Action</th>
-                                    </tr>
-                                    <?php $i = $AwalDataBarang + 1; ?>
-                                    <!-- Looping Array -->
-                                    <?php foreach ($Barang as $row) : ?>
+                                <table class="table table-striped text-center" id="table-1">
+                                    <thead>
                                         <tr>
-                                            <td><?= $i; ?></td>
-                                            <td><?= $row['Nama_barang']; ?></td>
-                                            <td><?= $row['Jenis_barang']; ?></td>
-                                            <td><?= $row['Jumlah']; ?></td>
-                                            <td>
-                                                <a href="UpdateBarang.php?id=<?= $row['id']; ?>" class="btn btn-warning">Update</a>
-                                                <button class="btn btn-danger" data-confirm="Realy?|Do you want to continue?" data-confirm-yes="window.location.replace('../backend/delete.php?id=<?= $row["id"]; ?>');">Delete</button>
-                                            </td>
+                                            <th>No</th>
+                                            <th>Nama Barang</th>
+                                            <th>Jenis Barang</th>
+                                            <th>Jumlah</th>
+                                            <th>Action</th>
                                         </tr>
-                                        <?php $i++; ?>
-                                    <?php endforeach; ?>
+                                    </thead>
+                                    <tbody>
+                                        <?php $i = 1; ?>
+                                        <!-- Looping Array -->
+                                        <?php foreach ($Barang as $row) : ?>
+                                            <tr>
+                                                <td><?= $i; ?></td>
+                                                <td><?= $row['Nama_barang']; ?></td>
+                                                <td><?= $row['Jenis_barang']; ?></td>
+                                                <td><?= $row['Jumlah']; ?></td>
+                                                <td>
+                                                    <a href="UpdateBarang.php?id=<?= $row['id']; ?>" class="btn btn-warning">Update</a>
+                                                    <button class="btn btn-danger" data-confirm="Realy?|Do you want to continue?" data-confirm-yes="window.location.replace('../backend/delete.php?id=<?= $row["id"]; ?>');">Delete</button>
+                                                </td>
+                                            </tr>
+                                            <?php $i++; ?>
+                                        <?php endforeach; ?>
+                                    </tbody>
                                 </table>
                             </div>
-                        </div>
-                        <!-- Pagination -->
-                        <div class="card-footer text-right">
-                            <nav class="d-inline-block">
-                                <ul class="pagination mb-0">
-                                    <?php if ($HalamanAktifBarang > 1) : ?>
-                                        <li class="page-item ">
-                                            <a class="page-link" href="?halaman=<?= $HalamanAktifBarang - 1; ?>" tabindex="-1"><i class="fas fa-chevron-left"></i></a>
-                                        </li>
-                                    <?php endif; ?>
-                                    <?php for ($i = 1; $i <= $JumlahHalamanBarang; $i++) : ?>
-                                        <?php if ($i == $HalamanAktifBarang) : ?>
-                                            <li class="page-item active"><a class="page-link" href="?halaman=<?= $i; ?>"><span class="sr-only">(current)</span><?= $i; ?></a></li>
-                                        <?php else : ?>
-                                            <li class="page-item">
-                                                <a class="page-link" href="?halaman=<?= $i; ?>"><?= $i; ?></a>
-                                            </li>
-                                        <?php endif; ?>
-                                    <?php endfor; ?>
-                                    <?php if ($HalamanAktifBarang < $JumlahHalamanBarang) : ?>
-                                        <li class="page-item">
-                                            <a class="page-link" href="?halaman=<?= $HalamanAktifBarang + 1; ?>"><i class="fas fa-chevron-right"></i></a>
-                                        </li>
-                                    <?php endif; ?>
-                                </ul>
-                            </nav>
                         </div>
                     </div>
                 </div>
