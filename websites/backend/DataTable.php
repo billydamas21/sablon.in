1a5aafe043fa9f23d185ec50761022ee9101d135
Farul Wananda <farulahmad2002@gmail.com>
2021-11-15 01:52:28 +0700

    edit query kelola ulasan

--- websites/backend/DataTable.php
+++ websites/backend/DataTable.php
@@ -4,17 +4,17 @@ require 'Functions.php';
 
 // Query Data akun + Jumlah data
 
-$KelolaAkun = query("SELECT * FROM users");
-$JumlahDataAkun = count(query("SELECT * FROM users"));
+$KelolaAkun = query("SELECT * FROM accounts");
+$JumlahDataAkun = count(query("SELECT * FROM accounts"));
 
 
 $Barang = query("SELECT * FROM kelola_barang");
 $JumlahDataBarang = count(query("SELECT * FROM kelola_barang"));
 
 
-$Riwayat = query("SELECT * FROM riwayat_barang");
-$JumlahDataRiwayat = count(query("SELECT * FROM riwayat_barang"));
+$Riwayat = query("SELECT * FROM kelola_riwayat");
+$JumlahDataRiwayat = count(query("SELECT * FROM kelola_riwayat"));
 
 
-$Ulasan = query("SELECT * FROM kelola_ulasan");
-$JumlahDataRiwayat = count(query("SELECT * FROM kelola_ulasan"));
+// $Ulasan = query("SELECT * FROM kelola_ulasan");
+// $JumlahDataRiwayat = count(query("SELECT * FROM kelola_ulasan"));
